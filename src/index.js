62be517cb68b0053dfbe6c7bbb99904b6145eca0
anushavan.hovsepyan <anushavan1993@gmail.com>
2021-03-22 21:23:02 +0400

    Clock impl

--- src/index.js
+++ src/index.js
@@ -1,16 +1,74 @@
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import React from 'react';
 import './index.css';
 
-const signInElement = (
-	<button>Sign In</button>
-);
+class Clock extends Component {
 
-const signUpElement = (
-	<button>Sign Up</button>
-);
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showTime: true
+		};
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				showTime: false
+			});
+		}, 5000);
+	}
+
+	render() {
+		const {date} = this.props;
+		const {showTime} = this.state;
+
+		return (
+			<>
+				<h1>Time</h1>
+				{showTime ? <Time date={date}/> : null}
+			</>
+		);
+	}
+}
+
+class Time extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentDate: props.date,
+			count: 0
+		};
+	}
+
+	componentDidMount() {
+		this.timerId = setInterval(() => {
+			const {currentDate} = this.state;
+			currentDate.setSeconds(currentDate.getSeconds() + 1);
+
+			this.setState({
+				currentDate: this.props.date
+			});
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+
+	render() {
+		const {currentDate} = this.state;
+
+		return (
+			<h1>{currentDate.toLocaleString()}</h1>
+		);
+	}
+}
 
 ReactDOM.render(
-	signUpElement,
+	<Clock date={new Date()}/>,
 	document.getElementById('root')
 );
